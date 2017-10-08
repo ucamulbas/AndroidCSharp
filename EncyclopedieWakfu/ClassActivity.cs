1601572d7cd111c8c7e7fdf16fae5fc1a3577011
ucamulbas <ucamulbas@hotmail.fr>
2017-10-08 16:43:52 +0200

    suppression d'une ligne

--- EncyclopedieWakfu/ClassActivity.cs
+++ EncyclopedieWakfu/ClassActivity.cs
@@ -22,7 +22,6 @@ namespace EncyclopedieWakfu
             base.OnCreate(savedInstanceState);
             classe = JsonConvert.DeserializeObject<Classe>(Intent.GetStringExtra("Classe"));
 
-
             // Create your application here
             SetContentView(Resource.Layout.ClassView);
             var className = FindViewById<TextView>(Resource.Id.classname);
@@ -33,7 +32,7 @@ namespace EncyclopedieWakfu
         private void Init()
         {
             var description = FindViewById<TextView>(Resource.Id.classdescription);
-            classe.description = GetString(Resources.GetIdentifier(classe.name.ToLower() + "description", "string",PackageName));
+            classe.description = GetString(Resources.GetIdentifier(string.Concat(classe.name.ToLower(), "description"), "string", PackageName));
             description.Text = classe.description;
         }
     }
